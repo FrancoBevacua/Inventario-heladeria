cc3c235713bc5a4fd799b38256acddfe3f03d667
FrancoBevacua <francobevacua@gmail.com>
2026-05-13 16:15:17 -0300

    database url data type fixed

--- backend/database.py
+++ backend/database.py
@@ -3,7 +3,7 @@ from fastapi import Depends
 from sqlmodel import SQLModel, create_engine, Session, Field, select
 
 #sql_filename: str = "database.db"
-DATABASE_URL: str = "postgresql://neondb_owner:npg_GsXLJxhKd63S@ep-withered-moon-aqz1ogz0-pooler.c-8.us-east-1.aws.neon.tech/neondb?channel_binding=require&sslmode=require"
+DATABASE_URL = "postgresql://neondb_owner:npg_GsXLJxhKd63S@ep-withered-moon-aqz1ogz0-pooler.c-8.us-east-1.aws.neon.tech/neondb?channel_binding=require&sslmode=require"
 
 connect_args = {"check_same_thread": False}
 engine = create_engine(DATABASE_URL, connect_args=connect_args)
