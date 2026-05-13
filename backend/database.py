4ad500a371edbf5a043c802047d2c469b66a3c0b
FrancoBevacua <francobevacua@gmail.com>
2026-05-12 23:09:12 -0300

    neon database implemented

--- backend/database.py
+++ backend/database.py
@@ -2,11 +2,11 @@ from typing import Annotated
 from fastapi import Depends
 from sqlmodel import SQLModel, create_engine, Session, Field, select
 
-sql_filename: str = "database.db"
-sqlite_url: str = f"sqlite:///{sql_filename}"
+#sql_filename: str = "database.db"
+DATABASE_URL: str = "postgresql://neondb_owner:npg_GsXLJxhKd63S@ep-withered-moon-aqz1ogz0-pooler.c-8.us-east-1.aws.neon.tech/neondb?channel_binding=require&sslmode=require"
 
 connect_args = {"check_same_thread": False}
-engine = create_engine(sqlite_url, connect_args=connect_args)
+engine = create_engine(DATABASE_URL, connect_args=connect_args)
 
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
