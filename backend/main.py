684467ebd453204eb7f4e6d97d18f82ce22c9d13
FrancoBevacua <francobevacua@gmail.com>
2026-05-24 01:44:50 -0300

    fix: render backend url fixed 2

--- backend/main.py
+++ backend/main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from database import create_db_and_tables
 from routers import helados
+from typing import List
 
 
 # Inicio de la aplicación (crear base de datos, tablas, etc)
@@ -13,18 +14,19 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
-origins: str = "https://francobevacua.github.io/inventario-heladeria/"
+origins: List[str] = [
+    "https://inventario-heladeria-git-develop-francos-projects-c738d0c4.vercel.app/",
+    "https://inventario-heladeria-backend.onrender.com/helados/"
+]
 
 # CORS - permitir solicitudes desde cualquier origen
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "https://inventario-heladeria-git-develop-francos-projects-c738d0c4.vercel.app/",
-        "https://inventario-heladeria-backend.onrender.com/helados/"
-        ],
+    allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allow_headers=["Authorization", "Content-Type", "Accept"],
+    max_age=3600
 )
 
 app.include_router(helados.router)
