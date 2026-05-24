0b78777f2354c7b61b1060938ece2f31b566c977
FrancoBevacua <francobevacua@gmail.com>
2026-05-24 02:54:11 -0300

    fix: comment out esta_disponible field and related endpoints in Helado model

--- backend/routers/helados.py
+++ backend/routers/helados.py
@@ -17,36 +17,37 @@ router = APIRouter(
 @router.get("/", response_model=List[HeladoReadWithLotes])
 def get_helados(session: SessionDep):
     helados = session.exec(select(Helado)).all()
+    print("Helados: ", helados)
     return helados
 
 # Consultar sólo los helados por disponibilidad
-@router.get("/disponibles")
-def get_helados_disponibles(session: SessionDep):
-    disponibles = session.exec(select(Helado.nombre).where(Helado.esta_disponible == True)).all()
-    no_disponibles = session.exec(select(Helado.nombre).where(Helado.esta_disponible == False)).all()
-    return {"Disponibles": disponibles, "No Disponibles": no_disponibles}
+#@router.get("/disponibles")
+#def get_helados_disponibles(session: SessionDep):
+#    disponibles = session.exec(select(Helado.nombre).where(Helado.esta_disponible == True)).all()
+#    no_disponibles = session.exec(select(Helado.nombre).where(Helado.esta_disponible == False)).all()
+#    return {"Disponibles": disponibles, "No Disponibles": no_disponibles}
 
 # Consultar disponibilidad por color
-@router.get("/disponibles/{color}")
-def get_helados_disponibles_por_color(color: str, session: SessionDep):
-    disponibles_por_color = session.exec(select(Helado).where(Helado.esta_disponible == True, Helado.color == color)).all()
-    nombres = [helado.nombre for helado in disponibles_por_color]
-    return {"Disponibles Color": nombres}
+#@router.get("/disponibles/{color}")
+#def get_helados_disponibles_por_color(color: str, session: SessionDep):
+#    disponibles_por_color = session.exec(select(Helado).where(Helado.esta_disponible == True, Helado.color == color)).all()
+#    nombres = [helado.nombre for helado in disponibles_por_color]
+#    return {"Disponibles Color": nombres}
 
 # Consultar disponibilidad por tipo
-@router.get("/{tipo}")
-def get_tipo(tipo: Tipos, session: SessionDep):
-    tipos_helados_disponibles = session.exec(select(Helado).where(Helado.esta_disponible == True, Helado.tipo == tipo)).all()
-    nombres = [helado.nombre for helado in tipos_helados_disponibles]
-    return {"Helados disponibles": nombres}
+#@router.get("/{tipo}")
+#def get_tipo(tipo: Tipos, session: SessionDep):
+#    tipos_helados_disponibles = session.exec(select(Helado).where(Helado.esta_disponible == True, Helado.tipo == tipo)).all()
+#    nombres = [helado.nombre for helado in tipos_helados_disponibles]
+#    return {"Helados disponibles": nombres}
 
 # POST - Crear nuevo helado
-@router.post("/")
-def crear_helado(helado: Helado, session: SessionDep) -> Helado:
-    session.add(helado)
-    session.commit()
-    session.refresh(helado)
-    return helado
+#@router.post("/")
+#def crear_helado(helado: Helado, session: SessionDep) -> Helado:
+#    session.add(helado)
+#    session.commit()
+#    session.refresh(helado)
+#    return helado
 
 # POST - Traspasos de lotes
 @router.post("/lotes/traspasar")
@@ -80,20 +81,20 @@ def traspasar_baldes(pedido: RequestTraspaso, session: Session = Depends(get_ses
 
 
 # PUT - Actualizar helado
-@router.put("/")
-def actualizar_helado(helado_id: int, nombre: str, color: str, tipo: Tipos, esta_disponible: bool, fecha_elaboracion: datetime.date, cantidad_baldes: int, session: Session = Depends(get_session)):
-    helado_encontrado = session.get(Helado, helado_id)
-    helado_encontrado.nombre = nombre
-    helado_encontrado.color = color
-    helado_encontrado.tipo = tipo
-    helado_encontrado.esta_disponible = esta_disponible
-    helado_encontrado.fecha_elaboracion = fecha_elaboracion
-    helado_encontrado.cantidad_baldes = cantidad_baldes
+#@router.put("/")
+#def actualizar_helado(helado_id: int, nombre: str, color: str, tipo: Tipos, esta_disponible: bool, fecha_elaboracion: datetime.date, cantidad_baldes: int, session: Session = Depends(get_session)):
+#    helado_encontrado = session.get(Helado, helado_id)
+#    helado_encontrado.nombre = nombre
+#    helado_encontrado.color = color
+#    helado_encontrado.tipo = tipo
+    #helado_encontrado.esta_disponible = esta_disponible
+#    helado_encontrado.fecha_elaboracion = fecha_elaboracion
+#    helado_encontrado.cantidad_baldes = cantidad_baldes
 
-    session.add(helado_encontrado)
-    session.commit()
-    session.refresh(helado_encontrado)
-    return helado_encontrado
+#    session.add(helado_encontrado)
+#    session.commit()
+#    session.refresh(helado_encontrado)
+#    return helado_encontrado
 
 # DELETE - Eliminar helado
 @router.delete("/")
