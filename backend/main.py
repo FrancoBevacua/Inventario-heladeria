e43ec904315d57967634daf889975029e5069c63
FrancoBevacua <francobevacua@gmail.com>
2026-05-24 01:12:06 -0300

    fix: allow origins from vercel website

--- backend/main.py
+++ backend/main.py
@@ -18,7 +18,7 @@ origins: str = "https://francobevacua.github.io/inventario-heladeria/"
 # CORS - permitir solicitudes desde cualquier origen
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://inventario-heladeria-git-develop-francos-projects-c738d0c4.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
